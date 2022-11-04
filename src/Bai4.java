3d4b25f1a41fbc641a090fe1dbe9f014314128d8
Sakurathanh2003 <96622230+Sakurathanh2003@users.noreply.github.com>
2022-11-04 18:17:58 +0700

    update bai4

--- src/Bai4.java
+++ src/Bai4.java
@@ -27,7 +27,7 @@ public class Bai4 {
         if (n <= 1) return false;
         if (n == 2 || n == 3) return true;
 
-        for (int i = 2; i * i < n; i++) {
+        for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) return false;
         }
         return true;
